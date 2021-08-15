b1bdaba9aa6831f0ca3e104f79d98e52063aa54b
David Fulham <42609423+mungonpo@users.noreply.github.com>
2021-08-14 23:15:09 -0700

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -24,33 +24,34 @@ for i in range(0, 8343242, 2):
     r1 = requests.get('https://www.abc.net.au/news/'+str(uid))
     if r1.status_code == 200:
 
-        # print(r1)
         coverpage = r1.content
         soup1 = BeautifulSoup(coverpage, 'html.parser')
         coverpage_news = soup1.find(
             'h1', class_='_3mBrr I7ej6 LTJIg _3qPMD _2hFDS _2Od9e _582YK _2eB4R _3Z8IO')
-        date_published = soup1.find('time',class_='W-g-R _14nkQ _3BwtN _2eB4R _3qdyT _3fa1F')
-        bi_line = soup1.find('span', class_='W-g-R _3XvRm _3BwtN _2eB4R _3qdyT _7kwJ9')
+        date_published = soup1.find(
+            'time', class_='W-g-R _14nkQ _3BwtN _2eB4R _3qdyT _3fa1F')
+        bi_line = soup1.find(
+            'span', class_='W-g-R _3XvRm _3BwtN _2eB4R _3qdyT _7kwJ9')
         body = soup1.find_all('p', class_='_1HzXw')
 
-        if ((coverpage_news is not None) and (coverpage_news.get_text().startswith(tuple(list_var))==False)):
+        if ((coverpage_news is not None) and (coverpage_news.get_text().startswith(tuple(list_var)) == False)):
             success_counter += 1
             print("Successful:" + str(success_counter))
             print("Total:" + str(i+1))
             headline = coverpage_news.get_text()
-            unique_id = uid
 
             if date_published is not None:
                 pub_date = date_published.get_text()
+            else:
+                pub_date = ''
 
             if bi_line is not None:
                 author = bi_line.get_text()
-            else: 
+            else:
                 author = ''
-
             if body is not None:
                 body_text = body
-            else: 
+            else:
                 body_text = ''
-
-            with nwks.append_table(values=["ABC News",headline, pub_date, unique_id,author,body_text])
+            nwks.append_table(
+                values=['ABC News', headline, pub_date, uid, author, body_text])
