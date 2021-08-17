31fe8e17779df09ef3ac52f16244f5791d7edbdc
David Fulham <42609423+mungonpo@users.noreply.github.com>
2021-08-17 03:28:18 -0700

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -10,32 +10,41 @@ c = pygsheets.authorize(service_account_env_var='GDRIVE_API_CREDENTIALS')
 list_var = {"AUDIO:", "IMAGE:", "VIDEO:"}
 start = 254246 #unique id from the URL from which to start scraping iterations
 
+## Check whether a sheet with this starting unique id already exists, and if not, create one, and open it
 try: 
   c.open('start_'+str(start))
 except:
   c.create('start_'+str(start))
 sh = c.open('start_'+str(start))
 
+## Set cell range and headers which will be pop
 google_sheet = sh.worksheet(property='index',value=0)
 google_sheet.resize(rows=100000,cols=6)
 google_sheet.append_table(
     values=['Source', 'title', 'published_at', 'UID', 'published_by', 'body'])
 
+## iterate through each article, and parse data 
+
 for i in range(4, 8343244, 2):
     uid = start+i
     r1 = requests.get('https://www.abc.net.au/news/'+str(uid),allow_redirects=5)
+    
+   ## Check the web url does not return an error
+ 
     if r1.status_code == 200:
         coverpage = r1.content
         soup1 = BeautifulSoup(coverpage, 'html.parser')
-        coverpage_news = soup1.find(
+        headline = soup1.find(
             'h1', class_='_3mBrr I7ej6 LTJIg _3qPMD _2hFDS _2Od9e _582YK _2eB4R _3Z8IO')
         date_published = soup1.find(
             'time', class_='W-g-R _14nkQ _3BwtN _2eB4R _3qdyT _3fa1F')
         bi_line = soup1.find(
             'span', class_='W-g-R _3XvRm _3BwtN _2eB4R _3qdyT _7kwJ9')
         body = soup1.find_all('p', class_='_1HzXw')
-        if ((coverpage_news is not None) and (coverpage_news.get_text().startswith(tuple(list_var)) == False)):
-            headline = coverpage_news.get_text()
+        
+        ## check that headline is present and does not indicate that headline does not start with list variable
+        if ((headline is not None) and (headline.get_text().startswith(tuple(list_var)) == False)):
+            headline = headline.get_text()
 
             if date_published is not None:
                 pub_date = date_published.get_text()
