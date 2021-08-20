c8b91127ce6f42adaed707672c0a7be990ee9a3d
David Fulham <42609423+mungonpo@users.noreply.github.com>
2021-08-19 22:02:32 -0700

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -28,7 +28,15 @@ google_sheet.append_table(
 
 for i in range(4, 8343244, 2):
     uid = start+i
-    r1 = requests.get('https://www.abc.net.au/news/'+str(uid),allow_redirects=5)
+    headers = {
+    'Accept-Encoding': 'gzip, deflate, sdch',
+    'Accept-Language': 'en-US,en;q=0.8',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',}
+    r1 = requests.get('https://www.abc.net.au/news/'+str(uid),allow_redirects=False,headers=headers)
     
    ## Check the web url does not return an error
  
