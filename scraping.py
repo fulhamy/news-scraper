9f56b39f055177bbc73267d298ae25a58216b52b
David Fulham <42609423+mungonpo@users.noreply.github.com>
2021-09-12 22:54:57 -0700

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -30,6 +30,7 @@ except:
 
 ## iterate through each article, and parse data 
 for i in range(0, 8343244, 2):
+    initial = wks.get_value('A1', value_render='UNFORMATTED_VALUE')
     uid = int(initial)+i
     headers = {
     'Accept-Encoding': 'gzip, deflate, sdch',
