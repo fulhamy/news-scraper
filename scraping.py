5970bcc3053542d8333529a5e3ed182827f3866c
David Fulham <42609423+mungonpo@users.noreply.github.com>
2021-09-27 16:15:12 -0700

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -1,68 +1,66 @@
-
-# module imports
 import requests
 from bs4 import BeautifulSoup
 from datetime import date
 import pygsheets
-from os import environ #environment variables from Heroku
+from os import environ  # environment variables from Heroku
 from sqlalchemy import create_engine
 from pandas import DataFrame
 
 c = pygsheets.authorize(service_account_env_var='GDRIVE_API_CREDENTIALS')
 
 list_var = {"AUDIO:", "IMAGE:", "VIDEO:"}
-start = environ.get('STARTING_VALUE') #environment variable defining the url uid at which to start iterating
+start = environ.get('STARTING_VALUE')  # environment variable defining the url uid at which to start iterating
 
-engine = create_engine(environ.get('DATABASE_URL'), echo = False)
+engine = create_engine(environ.get('DATABASE_URL'), echo=False)
 
-## create or open a file to store the most recent succesfully processed uid from the url, or the starting 
-try: 
-  ish = c.open('initial_'+str(start))
-  wks = ish.worksheet_by_title('Sheet1')
-  initial = wks.get_value('A1', value_render='UNFORMATTED_VALUE')
+# create or open a file to store the most recent succesfully processed uid from the url, or the starting
+try:
+    ish = c.open('initial_' + str(start))
+    wks = ish.worksheet_by_title('Sheet1')
+    initial = wks.get_value('A1', value_render='UNFORMATTED_VALUE')
 
 except:
-  c.create('initial_'+str(start))
-  ish = c.open('initial_'+str(start))
-  wks = ish.worksheet_by_title('Sheet1')
-  wks.update_value('A1', start)
-  ish.share('fulham.davidc@gmail.com',role='writer',type='user')
-  initial = wks.get_value('A1', value_render='UNFORMATTED_VALUE')
+    c.create('initial_' + str(start))
+    ish = c.open('initial_' + str(start))
+    wks = ish.worksheet_by_title('Sheet1')
+    wks.update_value('A1', start)
+    ish.share('fulham.davidc@gmail.com', role='writer', type='user')
+    initial = wks.get_value('A1', value_render='UNFORMATTED_VALUE')
 
-## iterate through each article, and parse data 
+# iterate through each article, and parse data
 for i in range(0, 8343244, 2):
     initial = wks.get_value('A1', value_render='UNFORMATTED_VALUE')
-    uid = int(initial)+int(i)
+    uid = int(initial) + int(i)
     headers = {
-    'Accept-Encoding': 'gzip, deflate, sdch',
-    'Accept-Language': 'en-US,en;q=0.8',
-    'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-    'Cache-Control': 'max-age=0',
-    'Connection': 'keep-alive',}
+        'Accept-Encoding': 'gzip, deflate, sdch',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/92.0.4515.131 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Cache-Control': 'max-age=0',
+        'Connection': 'keep-alive', }
 
     initial = wks.update_value('A1', uid)
     try:
-        r1 = requests.get('https://www.abc.net.au/news/'+str(uid),headers=headers,timeout=3.5,)
-        
-        
-       ## Check the web url does not return an error
-     
+        r1 = requests.get('https://www.abc.net.au/news/' + str(uid), headers=headers, timeout=3.5, )
+
+        # Check the web url does not return an error
+
         if r1.status_code == 200:
             coverpage = r1.content
             soup1 = BeautifulSoup(coverpage, 'html.parser')
-            headline = soup1.find(
-              'h1', class_='_24eQK _3O3fw _23WqP _2yZBa UOKdn _3I3Xh _2uBSR _10YQT _1lh6E')
-            date_published = soup1.find(
-              'time', class_='_3rsys _1srG4 _3PhF6 _10YQT _2Cu8q _1yU-k')
-            bi_line = soup1.find(
-             class_='_3rsys _1cBaI _3PhF6 _10YQT _2Cu8q _7kwJ9')
+            # headline = soup1.find('h1', class_='_24eQK _3O3fw _23WqP _2yZBa UOKdn _3I3Xh _2uBSR _10YQT _1lh6E')
+            headline = soup1.find('h1')
+            # date_published = soup1.find('time', class_='_3rsys _1srG4 _3PhF6 _10YQT _2Cu8q _1yU-k')
+            date_published = soup1.find('time')
+            bi_line = soup1.find(class_='_3rsys _1cBaI _3PhF6 _10YQT _2Cu8q _7kwJ9')
             body = soup1.find_all('p', class_='_1HzXw')
-            print("url called"+str(uid))
-            
-            ## check that headline is present and does not indicate that headline does not start with list variable
-            if ((headline is not None) and (headline.get_text().startswith(tuple(list_var)) == False)):
+            print("url called" + str(uid))
+
+            # check that headline is present and does not indicate that headline does not start with list variable
+            # here I just printed every var to see their value
+            if (headline is not None) and (headline.get_text().startswith(tuple(list_var)) is False):
                 headline = headline.get_text()
 
                 if date_published is not None:
@@ -75,21 +73,16 @@ for i in range(0, 8343244, 2):
                     author = None
                 if body is not None:
                     body_text = str(body)
-                else: 
+                else:
                     body_text = None
-                    
-                news_log = {}
-                news_log["Source"] = 'ABC News'
-                news_log["title"] = headline
-                news_log["published_at"] = pub_date
-                news_log["UID"] = uid
-                news_log["published_by"] = author
-                news_log["body"] = body_text
-                
-                df = DataFrame(data=news_log,index=[0])
-                
-                df.to_sql('news_log', con = engine, if_exists='append')
+
+                news_log = {"Source": 'ABC News', "title": headline, "published_at": pub_date, "UID": uid,
+                            "published_by": author, "body": body_text}
+
+                df = DataFrame(data=news_log, index=[0])
+
+                df.to_sql('news_log', con=engine, if_exists='append')
         else:
             continue
     except:
-      continue
+        continue
