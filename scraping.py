b04adf6782b1bb2850cdaa8cf815435124ec2f73
David Fulham <42609423+mungonpo@users.noreply.github.com>
2021-09-27 20:08:15 -0700

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -30,7 +30,7 @@ except:
 # iterate through each article, and parse data
 for i in range(0, 8343244, 2):
     
-    uid = int(initial) + int(i)
+    uid = int(initial) + i
     headers = {
         'Accept-Encoding': 'gzip, deflate, sdch',
         'Accept-Language': 'en-US,en;q=0.8',
